c8eaad60604fa2510069adb5b8090cc14b93be52
kirikaka <kiridodo0714@gmail.com>
2022-01-18 11:31:19 +0900

    move folder

---
+++ Algorithm/1026.py
@@ -0,0 +1,10 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=[]
+a.sort()
+for i in range(n):
+    loc=b.index(max(b))
+    max(b)*a[i]
+    b.pop(loc)
+    
